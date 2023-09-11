d4848e53b902c864571b8da0660ada780765238d
Nicholas <nicholasikiroma@gmail.com>
2023-09-11 11:04:12 +0100

    implement handlers

--- src/controllers/personController.js
+++ src/controllers/personController.js
@@ -33,7 +33,7 @@ export const createPerson = async (req, res) => {
 
   // Sanitize data
 
-  const person = new Person(data);
+  const person = new Person({ name });
   try {
     person.save();
     res.status(201).json({ data: person });
@@ -46,12 +46,50 @@ export const createPerson = async (req, res) => {
 //@route PUT /api
 //@access public
 export const updatePerson = async (req, res) => {
-  res.json({ message: "This is a put request" });
+  const { name } = req.body;
+  if (!name || typeof name !== "string") {
+    res.status(400).json({ error: "Invalid or missing 'name' field" });
+  }
+
+  try {
+    const updatedUser = await Person.findByIdAndUpdate(
+      req.params.userId,
+      { name },
+      {
+        new: true,
+      }
+    );
+    if (!updatedUser) {
+      res.status(404).json({ error: "User not found." });
+    } else {
+      res.status(200).json({ data: updatedUser });
+    }
+  } catch (err) {
+    if (err.name === "CastError") {
+      return res.status(400).json({ message: "Invalid ID format" });
+    }
+    return res.status(500).json({ error: err.message });
+  }
 };
 
 //@desc Delete a Person resource
 //@route DELETE /api
 //@access public
 export const deletePerson = async (req, res) => {
-  res.json({ message: "This is a delete request" });
+  const { userId } = req.params;
+  try {
+    const result = await Person.findByIdAndDelete(userId);
+
+    if (!result) {
+      res.status(404).json({ message: "User not found." });
+    } else {
+      res.json({ data: result });
+    }
+  } catch (err) {
+    // Check if the error is a CastError (invalid ID)
+    if (err.name === "CastError") {
+      return res.status(400).json({ message: "Invalid ID format" });
+    }
+    res.status(500).json({ message: err.message });
+  }
 };
